c7e8c18fbfb63724295f94aafc5cafcfbd5fbef1
James Bathgate <julesbravo@gmail.com>
2019-06-27 21:04:04 -0700

    {

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,36 @@
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+  <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+  <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>James Bathgate</h1>
+  <h1>James Bathgate</h1>
 
+  <p>
+    So I'm playing with Gridsome on Netlify and thinking of using it to build a new CV site.
+  </p>
+
+  <h2>Contact</h2>
+
+  <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
     <p>
-      So I'm playing with Gridsome on Netlify and thinking of using it to build a new CV site.
+      <label>Email: <input type="text" name="name" /></label>
     </p>
-
-    <h2>Contact</h2>
-
-    <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
     <p>
-        <label>Email: <input type="text" name="name" /></label>
-    </p>
-    <p>
-        <label>Message: <textarea name="message" class="message"></textarea></label>
+      <label>Message: <textarea name="message" class="message"></textarea></label>
     </p>
     <div data-netlify-recaptcha="true"></div>
     <p>
-        <button type="submit">Send</button>
+      <button type="submit">Send</button>
     </p>
-    </form>
+  </form>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Gridsome GitHub</a>
-      <a href="https://www.netlify.com" target="_blank" rel="noopener">Netlify</a>
-    </p>
+  <p class="home-links">
+    <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
+    <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Gridsome GitHub</a>
+    <a href="https://www.netlify.com" target="_blank" rel="noopener">Netlify</a>
+  </p>
 
   </Layout>
 </template>
@@ -37,7 +38,31 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Home'
+  title: 'Home'
+  },
+  data() {
+    return {
+      formData: {},
+    }
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    },
+    handleSubmit(e) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/success'))
+      .catch(error => alert(error))
+    }
   }
 }
 </script>
---
+++ src/pages/Success.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Thanks for the e-mail!</h1>
+</template>
