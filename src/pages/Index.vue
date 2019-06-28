05a43e75d566f671b54548e008ef828d9199563e
James Bathgate <julesbravo@gmail.com>
2019-06-27 20:10:21 -0700

    Initial commit for playing with Gridsome

---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>James Bathgate</h1>
+
+    <p>
+      So I'm playing with Gridsome on Netlify and thinking of using it to build a new CV site.
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">Gridsome GitHub</a>
+      <a href="https://www.netlify.com" target="_blank" rel="noopener">Netlify</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
